7550df10bab01361f7e5a317c17603cdf1a1b615
Sanjeev Gangwar <tragicomrade@gmail.com>
2023-12-30 21:02:14 +0530

    Updates

---
+++ precache-manifest.d0b63d368901f00f20e3162636f5d569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8cf3596d73ea25f8d3e24545f0d4115",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8c3a31686147498d29bd",
+    "url": "/Portfolio/static/css/main.22718620.chunk.css"
+  },
+  {
+    "revision": "612e55e952f864a03181",
+    "url": "/Portfolio/static/js/2.f0bb771f.chunk.js"
+  },
+  {
+    "revision": "8c3a31686147498d29bd",
+    "url": "/Portfolio/static/js/main.fd0f8a2f.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "f279311a26639e17265cafbb835a0be4",
+    "url": "/Portfolio/static/media/HeroImage.f279311a.jpg"
+  }
+]);
