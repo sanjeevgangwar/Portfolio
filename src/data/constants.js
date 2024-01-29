23ad09d9cb01298760b2ef658d41f4bb311920c8
Sanjeev Gangwar <tragicomrade@gmail.com>
2024-01-29 20:52:11 +0530

    resume updation

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sanjeevgangwar",
   resume:
-    "https://drive.google.com/drive/folders/1OeEacShnegQAekT5amE3KUDB75OADxCh?usp=sharing",
+    "https://drive.google.com/file/d/1cYjQ7CWBvaQhrByBIfcEvW26aEky8LWO/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/sanjeev-kumar983/",
   twitter: "https://twitter.com/sanjeevgangwarr",
   insta: "https://www.instagram.com/sanjeevgangwarr/",
